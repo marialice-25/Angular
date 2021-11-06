c3d8057e9e8981419a569339a74c871d7882501a
Maria Alice <78799728+marialice-25@users.noreply.github.com>
2021-11-06 14:44:01 -0300

    Add files via upload

---
+++ frontend/src/app/components/eletronics/eletronics-create/eletronics-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eletronics-create',
+  templateUrl: './eletronics-create.component.html',
+  styleUrls: ['./eletronics-create.component.css']
+})
+export class EletronicsCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
